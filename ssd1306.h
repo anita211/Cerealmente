58ccc2eddae61d0be8c0aaaf58e78d5280313691
Anita Oliveira <211068243@aluno.unb.br>
2025-02-19 11:54:07 -0300

    organizing ssd1306

--- ssd1306.h
+++ ssd1306.h
@@ -1,4 +1,6 @@
 #include "ssd1306_i2c.h"
+#include <stdbool.h>
+
 extern void calc_render_area_buflen(struct render_area *area);
 extern void SSD1306_send_cmd(uint8_t cmd);
 extern void SSD1306_send_cmd_list(uint8_t *buf, int num);
